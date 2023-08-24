815d07d8c3241a7dbe9709cfe78f459ab63ba6cf
TQuannn <1800002809@nttu.edu.vn>
2023-08-24 09:46:29 +0700

    update

--- Base/component.ts
+++ Base/component.ts
@@ -1,11 +1,9 @@
-import { Channel } from "../Models/Channel";
 import { BaseModel } from "./baseModel";
-  class Component<T extends BaseModel> {
-    Render(model: T): string {
-      const container =`<div><h1>${model.title}</h1><img src="${model.imgUrl}" alt="Image"><p>Like: ${model.like}, Unlike: ${model.unlike}</p><p>Channel: ${model.channel.name}</p></div>`;
-      return container;
-    }
+class Component<T extends BaseModel> {
+  Render(model: T): string {
+    const container = `<div><h1>${model.title}</h1><img src="${model.imgUrl}" alt="Image"><p>Like: ${model.like}, Unlike: ${model.unlike}</p><p>Channel: ${model.channel.name}</p></div>`;
+    return container;
   }
-  
-  export default Component;
-  
+}
+
+export default Component;
